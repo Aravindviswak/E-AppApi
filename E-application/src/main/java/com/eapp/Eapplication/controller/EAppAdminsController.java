57a2a1e73f69d4014baa035c5f39f6409534005a
Aravindviswak <aravindachary34@gmail.com>
2023-10-22 22:19:12 +0530

    third commit

--- E-application/src/main/java/com/eapp/Eapplication/controller/EAppAdminsController.java
+++ E-application/src/main/java/com/eapp/Eapplication/controller/EAppAdminsController.java
@@ -1,12 +1,15 @@
 package com.eapp.Eapplication.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +42,27 @@ public class EAppAdminsController {
 		}
 		return ResponseEntity.ok(data);
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<AdminEntity> getAdminById(@PathVariable Long id) throws DataNotFoundException {
+	    Optional<AdminEntity> data = service.getAdminById(id);
+
+	    return ResponseEntity.ok(data.get());
+	   
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateAdmin(@PathVariable Long id, @RequestBody AdminEntity updatedAdmin) {
+	    try {
+	        AdminEntity admin = service.updateAdmin(id, updatedAdmin);
+	        if (admin != null) {
+	            return ResponseEntity.ok(admin); // 200 OK with updated data
+	        } else {
+	            return ResponseEntity.notFound().build();// 404 Not Found if data doesn't exist
+	        }
+	    } catch (DataNotFoundException e) {
+	        return ResponseEntity.notFound().build(); // Handle the exception with 404 Not Found
+	    }
+	}
+
+
 }
--- E-application/src/main/java/com/eapp/Eapplication/service/EAppAdminsService.java
+++ E-application/src/main/java/com/eapp/Eapplication/service/EAppAdminsService.java
@@ -1,12 +1,15 @@
 package com.eapp.Eapplication.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.eapp.Eapplication.dao.EAppAdminsDAO;
 import com.eapp.Eapplication.entity.AdminEntity;
+import com.eapp.Eapplication.exception.DataNotFoundException;
 
 @Service
 public class EAppAdminsService {
@@ -22,4 +25,35 @@ public class EAppAdminsService {
 		return repository.findAll();
 	}
 
+	public Optional<AdminEntity> getAdminById(Long id) throws DataNotFoundException {
+	    Optional<AdminEntity> data= repository.findById(id);
+	    
+	    if (data.isPresent()) {
+	        return data; // Extract the AdminEntity from the Optional
+	    } else {
+	        throw new DataNotFoundException("No admin details are there with id " + id);
+	    }
+	}
+
+	public AdminEntity updateAdmin(Long id, AdminEntity updatedAdmin) throws DataNotFoundException {
+	    Optional<AdminEntity> optionalData = getAdminById(id);
+
+	    if (optionalData.isPresent()) {
+	        AdminEntity admin = optionalData.get();
+
+	        // Update the fields you want to modify
+	        
+	        admin.setAdmin_name(updatedAdmin.getAdmin_name());
+	        admin.setAdmin_email(updatedAdmin.getAdmin_email());
+	        admin.setAdmin_contact(updatedAdmin.getAdmin_contact());
+	        admin.setAdmin_password(updatedAdmin.getAdmin_password());
+
+	        return repository.save(admin);
+	    } else {
+	        throw new DataNotFoundException("AdminEntity with ID " + id + " not found.");
+	    }
+	}
+
 }
+	
+	
